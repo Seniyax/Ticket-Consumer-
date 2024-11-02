d684f183214b75e738358d03db250a08c58769be
Seniyax <151843498+Seniyax@users.noreply.github.com>
2024-11-02 14:39:15 +0530

    commit ; add Configuration,vendor,Customer,Ticketpool

---
+++ src/Configuration.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class Configuration {
+    private int num;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Ticketing System");
+
+        int TotalNumOfTickets = scanner.nextInt();
+        System.out.print("Enter the total number of tickets to be in");
+
+        int TicketReleaseRate = scanner.nextInt();
+        System.out.print("Enter ticket release rate");
+
+        int customerRetrievalRate = scanner.nextInt();
+        System.out.print("Enter the customer retrieval rate");
+
+        int maxTicketCapacity = scanner.nextInt();
+        System.out.print("Enter the ticket capacity");
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
